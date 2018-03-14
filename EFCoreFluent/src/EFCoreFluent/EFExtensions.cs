405aa45f36522e51aa14a49263e363efaf2346f2
Jeremy Sinclair <jeremy.sinclair@live.com>
2018-03-14 01:03:00 -0400

    -Updated Project Reference in Solution -Removed unneeded System.Text reference.

--- EFCoreFluent/src/EFCoreFluent/EFExtensions.cs
+++ EFCoreFluent/src/EFCoreFluent/EFExtensions.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
